3948a6a089ecd36d39f8dc4ef2afd034a78a7e2f
Justin Guyton <guyton.justin@gmail.com>
2017-02-22 10:19:24 -0600

    still testing db

---
+++ api/models/__init__.py
@@ -0,0 +1,1 @@
+from testprogram import User
--- app.py
+++ app.py
@@ -1,23 +1,14 @@
 import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from api.models import testprogram
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/rpdptest'
 db = SQLAlchemy(app)
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-
-    def __repr__(self):
-        return '<Name %r>' % self.name
 
 @app.route('/')
 def hello():
