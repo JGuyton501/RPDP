b74311779cd9158c4a9c0cd32116008fc6a7ab48
Justin Guyton <guyton.justin@gmail.com>
2017-02-22 00:32:27 -0600

    t4

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import os
 from flask import Flask, render_template
-from flask.ext.sqlalchemy import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = sqlalchemy(app)
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 @app.route('/')
 def hello():
