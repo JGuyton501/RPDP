7475c6ead677efe896b2d84c96a07d640fc1cb8e
Justin Guyton <guyton.justin@gmail.com>
2017-02-22 00:15:10 -0600

    lame 3

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 from flask import Flask, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import sqlalchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
+db = sqlalchemy(app)
 
 @app.route('/')
 def hello():
