e1d30cd22842ee313b607933cfe027807b9592cc
Justin Guyton <guyton.justin@gmail.com>
2017-03-27 15:53:29 -0500

    db

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.debug = True
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/raPlus'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/raPlus'
 
 #import modules after init app 
 db = SQLAlchemy(app) 
